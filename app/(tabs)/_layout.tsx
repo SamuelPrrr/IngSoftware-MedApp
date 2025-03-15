656c9830e9ae997e4227fbdad33f2f24819be917
SamuelPrrr <samuel_prr@samuMac.local>
2025-03-14 21:27:06 -0600

    UPDATE TabNavigation

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,11 @@
-import { Tabs, Redirect} from 'expo-router';
+import { Tabs } from 'expo-router';
 import React from 'react';
-import { ImageComponent, ImageSourcePropType, Platform } from 'react-native';
-import {Text, View, Image } from 'react-native'
-import {icons} from '../../constants';
+import { ImageSourcePropType, Platform } from 'react-native';
+import { Text, View, Image } from 'react-native';
+import { icons } from '../../constants'; // Exportar los iconos por medio de sus definiciones en constants
+import tailwindConfig from '../../tailwind.config'; // Exportar los estilos configurados en tailwind
 
-//TypeScrypt es un lenguaje estaticamente tipado por lo cual tengo que definir los props
+// TypeScript es un lenguaje estáticamente tipado por lo cual tengo que definir los props
 type TabIconProps = {
   icon: ImageSourcePropType; // Tipo para la propiedad `icon`
   color: string; // Tipo para la propiedad `color`
@@ -12,92 +13,83 @@ type TabIconProps = {
   focused: boolean; // Tipo para la propiedad `focused`
 };
 
-const TabIcon = ({icon, color, name, focused} : TabIconProps) => {
-  return(
-    <View className='items-center justify-center gap-2'> 
+const TabIcon = ({ icon, color, name, focused }: TabIconProps) => {
+  return (
+    <View className='items-center justify-center gap-2' style={{ width: 80}}>
       <Image
         source={icon}
         resizeMode='contain'
         tintColor={color}
         className='w-6 h-6'
-      />
-      <Text className={`${focused ? 'font-semibold' : 'font-normal'} text-xs`}>
+      />  
+      <Text
+        className={`${focused ? 'font-extrabold' : 'font-normal'} text-xs`}
+        style={{ color: color}}
+      >
         {name}
       </Text>
     </View>
-  )
-}
-
+  );
+};
 
 export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
         tabBarShowLabel: false,
+        tabBarActiveTintColor: '#1372DF',
+        tabBarInactiveTintColor: '#CDCDE0',
         headerShown: false,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Usa un fondo transparente en iOS para mostrar el efecto de desenfoque
-            position: 'absolute',
-          },
-          default: {},
-        }),
+        tabBarStyle: {
+          backgroundColor: '#161622',
+          paddingTop: 10,
+          borderTopWidth: 1,
+          height: 80,
+        },
       }}>
       <Tabs.Screen
         name="book"
         options={{
           title: 'Agendar',
-          //Añadir icono
-          tabBarIcon: ({color, focused}) => (
-              <TabIcon
-                icon={icons.plus}
-                color={color}
-                name="Agendar"
-                focused={focused}
-              />
-          )
+          tabBarIcon: ({ color, focused }) => (
+            <TabIcon
+              icon={icons.plus}
+              color={color}
+              name="Agendar"
+              focused={focused}
+            />
+          ),
         }}
       />
       <Tabs.Screen
         name="profile"
         options={{
           title: 'Perfil',
-          tabBarIcon: ({color, focused}) => (
+          tabBarIcon: ({ color, focused }) => (
             <TabIcon
               icon={icons.profile}
               color={color}
               name="Perfil"
               focused={focused}
             />
-          )
+          ),
         }}
       />
       <Tabs.Screen
         name="settings"
         options={{
           title: 'Ajustes',
-          tabBarIcon: ({color, focused}) => (
+          tabBarIcon: ({ color, focused }) => (
             <TabIcon
               icon={icons.bookmark}
               color={color}
               name="Ajustes"
               focused={focused}
             />
-          )
+          ),
         }}
       />
     </Tabs>
   );
 }
 
-//Estilos de contenedotes, etc, etc
-const styles = {
-  tabContainer: {
-    flexDirection: 'row', // Asegura que el texto se comporte correctamente en una fila
-    alignItems: 'center', // Centra verticalmente el texto
-  },
-  tabText: {
-    flexShrink: 1, // Permite que el texto se reduzca para caber en el espacio disponible
-    flexWrap: 'wrap', // Permite que el texto se envuelva en múltiples líneas si es necesario
-  },
-};
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,14 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#1372DF',
+        primaryHover: '#093970',
+        primary50: '#D6E7FB',
+        primary100: '#84B8F4'
+      }
+    },
   },
   plugins: [],
 }
